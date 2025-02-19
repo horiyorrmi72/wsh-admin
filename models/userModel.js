a1c2bd055075d0484881b5fff03f1686e0bbff3b
yusuf <horiyorrmi72@gmail.com>
2025-02-19 15:46:31 +0100

    feat(role):add role to schema

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,14 @@ const userSchema = new Schema({
 	name: { type: String, required: true, trim: true },
 	email: { type: String, required: true, unique: true, trim: true },
 	password: { type: String, required: true, trim: true, min: 6 },
+	role: {
+		type: String,
+		required: true,
+		trim: true,
+		lowercase: true,
+		enum: ['admin', 'user'],
+		default: 'user',
+	},
 });
 const User = mongoose.model('User', userSchema);
 module.exports = User;
