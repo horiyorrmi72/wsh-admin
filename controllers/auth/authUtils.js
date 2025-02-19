9c6995055e328c1f579358dde86c5c15174b9700
yusuf <horiyorrmi72@gmail.com>
2025-02-19 15:36:29 +0100

    feat(auth guards):add guards to auth for authentication and authorization

--- controllers/auth/authUtils.js
+++ controllers/auth/authUtils.js
@@ -13,13 +13,79 @@ const compareData = async (plainData, hashedData) => {
 	return await bcrypt.compare(plainData, hashedData);
 };
 
-const generateToken = async (payload, expiryPeriod) =>{
+const generateToken = async (payload, expiryPeriod) => {
 	return jwt.sign(payload, jwt_secret, {
 		expiresIn: expiryPeriod,
 	});
+};
+
+const verifyToken = (token) => {
+	try {
+		return jwt.verify(token, jwt_secret);
+	} catch (error) {
+		return null;
+	}
+};
+
+const extractToken = (req) => {
+	const authHeader = req.headers.authorization;
+	// console.log(authHeader);
+	if (!authHeader || !authHeader.startsWith('Bearer ')) {
+		return null;
+	}
+	return authHeader.split(' ')[1];
+};
+
+const isAdmin = async (req, res, next) => {
+	try {
+		const token = extractToken(req);
+		if (!token) {
+			return res.status(401).json({ message: 'Access Denied.' });
+		}
+		const decodedToken = verifyToken(token);
+		if (!decodedToken) {
+			return res.status(401).json({ message: 'Invalid token.' });
+		}
+		if (decodedToken.role !== 'admin')
+		{
+			console.log(decodedToken.role);
+			return res.status(403).json({
+				message: `You don't have the priviledge to access this page.`,
+			});
+		}
+		req.user = decodedToken;
+		next();
+	} catch (error) {
+		return res
+			.status(500)
+			.json({ message: 'Internal Server Error', error: error.message });
+	}
+};
+const auth = async (req, res, next) => {
+	try {
+		const token = extractToken(req);
+		if (!token) {
+			return res.status(401).json({
+				message: 'Access Denied',
+			});
+		}
+		const decoded = verifyToken(token);
+		if (!decoded) {
+			return res.status(401).json({ message: 'Invalid credentials' });
+		}
+		req.user = decoded;
+		next();
+	} catch (error) {
+		return res
+			.status(500)
+			.json({ message: 'Internal Server Error', error: error.message });
+	}
 };
 module.exports = {
 	hashData,
 	compareData,
 	generateToken,
+	verifyToken,
+	isAdmin,
+	auth,
 };
