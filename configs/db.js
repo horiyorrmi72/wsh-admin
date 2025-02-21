ac9333f58ed788462f45fc7c4e74b72a744dc865
yusuf <horiyorrmi72@gmail.com>
2025-02-21 12:10:20 +0100

    feat(seeding admin): seed the admin details

--- configs/db.js
+++ configs/db.js
@@ -1,16 +1,50 @@
 const mongoose = require('mongoose');
 const { db_variables } = require('./configVariables');
+const { hashData } = require('../controllers/auth/authUtils');
+const User = require('../models/userModel');
+
 const URL = db_variables.dbUrl;
+
+const adminData = {
+	name: 'Admin',
+	email: 'admin@example.com',
+	password: 'Admin@1234',
+	role: 'admin',
+};
 
 const connectDb = async () => {
 	try {
 		await mongoose.connect(URL, {
 			dbName: 'wsh',
 		});
-		console.log('Connected to the database successfully!');
+		console.log('✅ Connected to the database successfully!');
+
+		// create admin after successful connection
+		await seedAdmin();
 	} catch (err) {
-		console.error(`Error connecting to the database: ${err}`);
+		console.error(`❌ Error connecting to the database: ${err.message}`);
 		process.exit(1);
+	}
+};
+
+// Seeding function to create admin if admin doesn't exist on the db
+const seedAdmin = async () => {
+	try {
+		const existingAdmin = await User.findOne({ role: 'admin' });
+		if (existingAdmin) {
+			// console.log('✅ Admin user already exists.');
+			return;
+		}
+
+		// Hashing the password before saving
+		const hashedPassword = await hashData(adminData.password);
+		adminData.password = hashedPassword;
+
+		// Creating the admin user
+		await User.create(adminData);
+		console.log('🎉 Admin user created successfully!');
+	} catch (error) {
+		console.error('❌ Error seeding admin:', error.message);
 	}
 };
 
