dd3be596f5ba3e46ff8123ac20c0522782cfe53b
yusuf <horiyorrmi72@gmail.com>
2025-02-21 12:11:29 +0100

    server fle

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const express = require('express');
-const route  = require('./routes');
+const route = require('./routes');
+const cors = require('cors');
+const session = require('express-session'); 
+const cookieParser = require('cookie-parser');
 const connectDb = require('./configs/db');
 
 connectDb();
 
 const app = express();
+const corsOptions = {
+	origin: 'https://wsh-five.vercel.app',
+};
+app.use(cookieParser());
+app.use(
+	session({
+		secret: 'my_secret_key',
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: true },
+	})
+);
 
+app.set('view engine', 'ejs');
+app.use(cors(corsOptions))
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', route);
+app.get('/login', (req, res) => {
+	res.render('login', { error: null });
+});
 
-
+app.get('/dashboard', (req, res) => {
+	const token = req.cookies.token; 
+	if (!token) {
+		return res.redirect('/login');
+	}
+	res.render('dashboard', { email });
+});
 module.exports = app;
