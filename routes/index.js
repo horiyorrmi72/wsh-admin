94cad123d41e72dd530d37b9a5b995848b781140
yusuf <horiyorrmi72@gmail.com>
2025-03-14 16:17:07 +0100

    feat(payment gateway):add flutterwave payment to accept products payments

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const authRoute = require('../controllers/auth/authRoute');
 const userRoute = require('../controllers/user/userRoute');
 const eventsRoute = require('../controllers/events/eventsRoute');
+const paymentRoute = require('../controllers/payments/paymentRoute');
 
 router.get('/', (req, res) => res.send('Hello World!'));
 router.use('/auth', authRoute);
 router.use('/users', userRoute);
 router.use('/events', eventsRoute);
+router.use('/payments', paymentRoute);
 
 module.exports = router;
