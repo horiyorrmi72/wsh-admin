f99f8587f204dac4d411a0ced11a24ebb851f9a7
yusuf <horiyorrmi72@gmail.com>
2025-03-17 12:54:12 +0100

    feat(payment Initiation):add payment initiation

--- controllers/payments/paymentRoute.js
+++ controllers/payments/paymentRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { verify } = require('./payment_verification');
+const { verify, initiatePayments } = require('./payment_verification');
 
 router.post('', (req, res) =>
 	res.send('Kindly use the other endpoint to verify the payment details.')
 );
+router.post('/initiate', initiatePayments);
 router.post('/verify', verify);
 
 module.exports = router;
