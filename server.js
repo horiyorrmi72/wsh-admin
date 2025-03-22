387e984619a03321b69721dc46c9619188a4a062
yusuf <horiyorrmi72@gmail.com>
2025-03-22 23:29:47 +0100

    fix(cors origin): cors crenditials

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const route = require('./routes');
 const cors = require('cors');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
 const connectDb = require('./configs/db');
 const bodyParser = require('body-parser');
 const axios = require('axios');
@@ -12,16 +11,19 @@ connectDb();
 
 const app = express();
 const corsOptions = {
-	origin: '*',
-	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-	preflightContinue: false,
+	origin: process.env.CLIENT_URL || 'http://localhost:4200',
+	methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
 	optionsSuccessStatus: 204,
+	preflightContinue: false,
 };
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+
 app.use(
 	session({
 		secret: process.env.SESSION_SECRET,
