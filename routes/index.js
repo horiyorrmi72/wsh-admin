5f409d442950b11cf6e268ea74752f08c845d929
yusuf <horiyorrmi72@gmail.com>
2025-03-23 06:49:49 +0100

    feat(publication route):add publication route

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const authRoute = require('../controllers/auth/authRoute');
 const userRoute = require('../controllers/user/userRoute');
 const eventsRoute = require('../controllers/events/eventsRoute');
 const paymentRoute = require('../controllers/payments/paymentRoute');
+const publicationRoute = require('../controllers/publications/publicationsRoute');
 
 router.get('/', (req, res) => res.send('Hello World!'));
 router.use('/auth', authRoute);
 router.use('/users', userRoute);
 router.use('/events', eventsRoute);
 router.use('/payments', paymentRoute);
+router.use('/publications',publicationRoute);
 
 module.exports = router;
