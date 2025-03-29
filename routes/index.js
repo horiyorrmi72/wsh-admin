57c595d11ba2d739b16e9c30fea3360775be6815
yusuf <horiyorrmi72@gmail.com>
2025-03-29 15:27:14 +0100

    feat(mail router): add email routes to the route list

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,15 @@ const userRoute = require('../controllers/user/userRoute');
 const eventsRoute = require('../controllers/events/eventsRoute');
 const paymentRoute = require('../controllers/payments/paymentRoute');
 const publicationRoute = require('../controllers/publications/publicationsRoute');
+const mailRoute = require('../controllers/mail/mailRoute');
 
 router.get('/', (req, res) => res.send('Hello World!'));
 router.use('/auth', authRoute);
 router.use('/users', userRoute);
 router.use('/events', eventsRoute);
 router.use('/payments', paymentRoute);
-router.use('/publications',publicationRoute);
+router.use('/publications', publicationRoute);
+router.use('/mailer', mailRoute);
+
 
 module.exports = router;
